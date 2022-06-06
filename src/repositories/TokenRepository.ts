1928f9065b4dca833a696a3eb527acd06068ec9e
Eugene <galeaf11@gmail.com>
2022-06-06 13:13:34 +0600

    feat: 🎸 refactor TokenService.ts

---
+++ src/repositories/TokenRepository.ts
@@ -0,0 +1,35 @@
+import DBService from '../services/DBService';
+
+export class TokenRepository {
+  private dbService: DBService;
+  private db;
+
+  constructor() {
+    this.dbService = DBService.getInstance();
+    this.db = DBService.getInstance().getTokenDB();
+  }
+
+  public async getUserTokens(userId: number): Promise<string[]> {
+    try {
+      return await this.db.get(String(userId));
+    } catch (e) {
+      if (e.status === 404) {
+        return [];
+      }
+      throw e;
+    }
+  }
+
+  public async putUserTokens(
+    userId: string,
+    verifiedTokens: string[]
+  ): Promise<void> {
+    await this.db.put(userId, verifiedTokens);
+  }
+
+  public async deleteUserTokens(userId: string): Promise<void> {
+    await this.db.del(String(userId));
+  }
+}
+
+export default new TokenRepository();
