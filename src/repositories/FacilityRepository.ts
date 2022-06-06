a564a5aeef5ae062f6e7626fc2a4a10532bbdcbf
Eugene <galeaf11@gmail.com>
2022-06-06 12:53:24 +0600

    feat: 🎸 refactor FacilityService.ts and UserService.ts

--- src/repositories/FacilityRepository.ts
+++ src/repositories/FacilityRepository.ts
@@ -1,6 +1,6 @@
-import DBService from '../services/DBService';
+import DBService, { FacilityItemType, FacilityLevelValues, FacilitySpaceLevelValues } from '../services/DBService';
 import { Level } from 'level';
-import { Facility } from '../proto/facility';
+import { Item } from '../proto/facility';
 
 export class FacilityRepository {
   private dbService: DBService;
@@ -24,18 +24,7 @@ export class FacilityRepository {
     return [];
   }
 
-  public async createFacility(
-    facilityId: string,
-    facility: Facility
-  ): Promise<void> {
-    const facilitySublevel = this.dbService.getFacilitySublevelDB(facilityId);
-
-    await facilitySublevel.put('metadata', facility);
-
-    await this.createFacilityIndex(facilityId);
-  }
-
-  private async createFacilityIndex(facilityId: string) {
+  public async createFacilityIndex(facilityId: string) {
     const facilitiesIds = await this.getFacilityIds();
 
     if (facilitiesIds.length > 0) {
@@ -46,4 +35,99 @@ export class FacilityRepository {
       await this.db.put('facilities', [facilityId]);
     }
   }
+
+  public async getItemIds(
+    facilityId: string,
+    itemType: FacilityItemType
+  ): Promise<string[]> {
+    try {
+      const facilitySublevel = this.dbService.getFacilitySublevelDB(facilityId);
+      return await facilitySublevel.get<string, string[]>(itemType, {
+        valueEncoding: 'json'
+      });
+    } catch (e) {
+      if (e.status !== 404) {
+        throw e;
+      }
+    }
+    return [];
+  }
+
+  public async createFacilityItem(
+    facilityId: string,
+    key: string,
+    value: FacilityLevelValues
+  ): Promise<void> {
+    const facilitySublevel = this.dbService.getFacilitySublevelDB(facilityId);
+
+    await facilitySublevel.put(key, value);
+  }
+
+  public async getFacilityDbKey(
+    facilityId: string,
+    key: string
+  ): Promise<FacilityLevelValues> {
+    try {
+      return await this.dbService.getFacilitySublevelDB(facilityId).get(key);
+    } catch (e) {
+      if (e.status === 404) {
+        throw new Error(`Unable to get "${key}" of facility "${facilityId}"`);
+      }
+      throw e;
+    }
+  }
+
+  public async getSpaceDbKey(
+    facilityId: string,
+    spaceId: string,
+    key: string
+  ): Promise<FacilitySpaceLevelValues> {
+    try {
+      return await this.dbService
+        .getFacilityItemDB(facilityId, 'spaces', spaceId)
+        .get(key);
+    } catch (e) {
+      if (e.status === 404) {
+        throw new Error(
+          `Unable to get "${key}" of space "${spaceId}" of facility "${facilityId}"`
+        );
+      }
+      throw e;
+    }
+  }
+
+  public async createSpaceIndex(
+    facilityId: string,
+    itemType: FacilityItemType,
+    itemId: string
+  ): Promise<void> {
+    const itemIds = await this.getItemIds(facilityId, itemType);
+    const facilitySublevel = this.dbService.getFacilitySublevelDB(facilityId);
+
+    if (itemIds.length > 0) {
+      const spaceSet = new Set<string>(itemIds);
+      spaceSet.add(itemId);
+      await facilitySublevel.put(itemType, Array.from(spaceSet));
+    } else {
+      await facilitySublevel.put(itemType, [itemId]);
+    }
+  }
+
+  public async createSpaceItem(
+    facilityId: string,
+    itemType: FacilityItemType,
+    itemId: string,
+    key: string,
+    value: Item | FacilitySpaceLevelValues
+  ): Promise<void> {
+    const sublevel = this.dbService.getFacilityItemDB(
+      facilityId,
+      itemType,
+      itemId
+    );
+
+    await sublevel.put(key, value);
+  }
 }
+
+export default new FacilityRepository();
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,29 +1,26 @@
 import { AppRole, User, UserDTO } from '../types';
-import DBService from './DBService';
 import bcrypt from 'bcrypt';
 import tokenService, { TokenService } from './TokenService';
 import ApiError from '../exceptions/ApiError';
 import { defaultManagerLogin } from '../config';
 import { MetricsService } from './MetricsService';
+import userRepository, { UserRepository } from '../repositories/UserRepository';
+import mainRepository, { MainRepository } from '../repositories/MainRepository';
 
 export class UserService {
-  private db;
-  private loginDB;
-  private dbService: DBService;
-  private mainDB;
   private tokenService: TokenService;
+  private repository: UserRepository;
+  private mainRepository: MainRepository;
 
   constructor() {
-    this.dbService = DBService.getInstance();
-    this.db = this.dbService.getUserDB();
-    this.mainDB = this.dbService.getDB();
-    this.loginDB = this.dbService.getLoginDB();
     this.tokenService = tokenService;
+    this.repository = userRepository;
+    this.mainRepository = mainRepository;
   }
 
   public async getAllUsers() {
     const users = new Set<UserDTO>();
-    const dbUsers: User[] = await this.db.values().all();
+    const dbUsers: User[] = await this.repository.getAllUsers();
 
     dbUsers.map((i) => {
       const userDTO = this.getUserDTO(i);
@@ -38,62 +35,34 @@ export class UserService {
     password: string,
     roles: AppRole[]
   ): Promise<void> {
-    const userExists = await this.getUserIdByLogin(login);
+    const userExists = await this.repository.getUserIdByLogin(login);
     if (userExists) {
       throw ApiError.BadRequest('User already exists');
     }
 
-    const id = await this.getId();
+    const id = await this.mainRepository.getId();
     const rounds = 2;
     const hashedPassword = await bcrypt.hash(String(password), rounds);
-    await this.db.put(String(id), {
+    await this.repository.createUser(
       id,
       login,
-      password: hashedPassword,
+      hashedPassword,
       roles
-    });
+    );
 
-    await this.loginDB.put(login, String(id));
-    await this.mainDB.put('user_db_increment', id);
+    await this.mainRepository.setUserDBIncrement(String(id));
 
     if (login !== defaultManagerLogin && roles.includes(AppRole.MANAGER)) {
       await this.deleteDefaultManagerAccount();
     }
   }
 
-  private async getId(): Promise<number> {
-    try {
-      return (await this.mainDB.get('user_db_increment')) + 1;
-    } catch (e) {
-      if (e.status === 404) {
-        return 1;
-      }
-      throw e;
-    }
-  }
-
-  public async getUserIdByLogin(login: string): Promise<number | null> {
-    try {
-      const userId = await this.loginDB.get(login);
-      return Number(userId);
-    } catch (e) {
-      if (e.status === 404) {
-        return null;
-      }
-      throw e;
-    }
-  }
-
-  public async getUserById(id: number): Promise<User> {
-    return await this.db.get(String(id));
-  }
-
   public async getUserByLogin(login: string): Promise<User | null> {
-    const id = await this.getUserIdByLogin(login);
+    const id = await this.repository.getUserIdByLogin(login);
     if (!id) {
       return null;
     }
-    return await this.getUserById(id);
+    return await this.repository.getUserById(id);
   }
 
   public getUserDTO(user: User): UserDTO {
@@ -106,11 +75,10 @@ export class UserService {
 
   public async deleteUser(id: number): Promise<void> {
     try {
-      const user = await this.getUserById(id);
+      const user = await this.repository.getUserById(id);
       const login = user.login;
 
-      await this.db.del(String(id));
-      await this.loginDB.del(login);
+      await this.repository.deleteUser(String(id), login);
       await this.tokenService.revokeAllUserTokens(id);
     } catch (e) {
       if (e.status !== 404) {
@@ -175,7 +143,7 @@ export class UserService {
       throw ApiError.UnauthorizedError();
     }
 
-    const user = await this.getUserById(data.id);
+    const user = await this.repository.getUserById(data.id);
     const userDTO = this.getUserDTO(user);
     const tokens = this.tokenService.generateTokens(userDTO);
     await this.tokenService.revokeToken(refreshToken);
@@ -188,7 +156,7 @@ export class UserService {
   }
 
   private async deleteDefaultManagerAccount(): Promise<void> {
-    const managerId = await this.getUserIdByLogin(defaultManagerLogin);
+    const managerId = await this.repository.getUserIdByLogin(defaultManagerLogin);
 
     if (managerId) {
       await this.deleteUser(managerId);
@@ -200,11 +168,11 @@ export class UserService {
     password: string
   ): Promise<void> {
     try {
-      const user = await this.getUserById(userId);
+      const user = await this.repository.getUserById(userId);
       const rounds = 2;
       user.password = await bcrypt.hash(String(password), rounds);
 
-      await this.db.put(String(userId), user);
+      await this.repository.updateUser(String(userId), user);
     } catch (e) {
       if (e.status === 404) {
         throw ApiError.BadRequest('User not found');
@@ -218,10 +186,10 @@ export class UserService {
     roles: AppRole[]
   ): Promise<void> {
     try {
-      const user = await this.getUserById(userId);
+      const user = await this.repository.getUserById(userId);
       user.roles = roles;
 
-      await this.db.put(String(userId), user);
+      await this.repository.updateUser(String(userId), user);
     } catch (e) {
       if (e.status === 404) {
         throw ApiError.BadRequest('User not found');
