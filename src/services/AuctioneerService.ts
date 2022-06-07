d3f5da26c8d00f9912f64727e64a5fb4d12d784a
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-06-07 13:22:14 +0200

    feat: 🎸 Interfaces for SearchService, AuctioneerService

---
+++ src/services/AuctioneerService.ts
@@ -0,0 +1,6 @@
+export class AuctioneerService {
+  constructor(where: string[]) {
+    // Listen to waku and contentTopics from where when listening for a an Ask.
+    // Monitor Stays events, filtering by facilityId on chain for any Deals that have been done.
+  }
+}
