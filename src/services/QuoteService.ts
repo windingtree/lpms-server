dcb8fa1956b1a8a8c5132c9647a7c99f4a274c64
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-06-08 17:27:49 +0200

    feat: 🎸 Added application of occupancy midifier; Added tests
    ✅ Closes: #46

--- src/services/QuoteService.ts
+++ src/services/QuoteService.ts
@@ -17,63 +17,60 @@ import {
 } from '../repositories/ModifierRepository';
 
 export interface QuoteRepositories {
-  ratesRepository: SpaceRateRepository;
-  spaceModifiersRepository: SpaceModifierRepository;
-  facilityModifierRepository: FacilityModifierRepository;
+  rates: SpaceRateRepository;
+  modifiers: SpaceModifierRepository;
+  facilityModifiers: FacilityModifierRepository;
 }
 
 export class QuoteService {
-  // Returns a quote for the Ask
-  public quote = async (
-    facilityId: string,
-    spaceId: string,
-    ask: Ask
+  // Get base rate for the space
+  static getBaseRate = async (
+    { rates }: QuoteRepositories,
+    day: DateTime
   ): Promise<BigNumber> => {
-    if (!ask.checkIn) {
-      throw new Error('Invalid checkIn date');
+    let baseRate: Rates | null;
+
+    try {
+      baseRate = await rates.getRate(
+        day.toFormat('yyyy-MM-dd') as FormattedDate
+      );
+    } catch (e) {
+      if (e.status !== 404) {
+        throw e;
+      }
+
+      baseRate = await rates.getRate('default');
     }
 
-    if (!ask.checkOut) {
-      throw new Error('Invalid checkOut date');
+    if (!baseRate) {
+      throw new Error('Unable to get base rate for the space');
     }
 
-    const repositories: QuoteRepositories = {
-      ratesRepository: new SpaceRateRepository(facilityId, spaceId),
-      spaceModifiersRepository: new SpaceModifierRepository(
-        facilityId,
-        spaceId
-      ),
-      facilityModifierRepository: new FacilityModifierRepository(facilityId)
-    };
-    let total = BigNumber.from(0);
+    return BigNumber.from(baseRate.cost);
+  };
 
-    // Iterate through days from ask.checkIn to ask.checkOut
-    for await (const value of this.getQuoteIterator(
-      repositories,
-      ask.checkIn,
-      ask.checkOut
-    )) {
-      total = total.add(value);
-    }
-
-    // Apply length_of_stay modifier, in order of priority
+  // Apply LOSRateModifier modifier
+  static applyLosRateModifier = async (
+    rate: BigNumber,
+    { modifiers, facilityModifiers }: QuoteRepositories,
+    checkIn: Date,
+    checkOut: Date
+  ): Promise<BigNumber> => {
     let losSpaceModifier: undefined | LOSRateModifier;
 
     try {
-      losSpaceModifier =
-        (await repositories.spaceModifiersRepository.getModifier(
-          'length_of_stay'
-        )) as LOSRateModifier;
+      losSpaceModifier = (await modifiers.getModifier(
+        'length_of_stay'
+      )) as LOSRateModifier;
     } catch (e) {
       if (e.status !== 404) {
         throw e;
       }
 
       try {
-        losSpaceModifier =
-          (await repositories.facilityModifierRepository.getModifier(
-            'length_of_stay'
-          )) as LOSRateModifier;
+        losSpaceModifier = (await facilityModifiers.getModifier(
+          'length_of_stay'
+        )) as LOSRateModifier;
       } catch (e) {
         if (e.status !== 404) {
           throw e;
@@ -82,8 +79,9 @@ export class QuoteService {
     }
 
     if (losSpaceModifier) {
-      const daysTotal = DateTime.fromObject(ask.checkOut).diff(
-        DateTime.fromObject(ask.checkIn)
+      const daysTotal = DateTime.fromObject(checkOut).diff(
+        DateTime.fromObject(checkIn),
+        'days'
       ).days;
       let applicable = false;
 
@@ -109,127 +107,204 @@ export class QuoteService {
       if (applicable) {
         switch (losSpaceModifier.valueOneof.oneofKind) {
           case 'ratio':
-            total = total
+            rate = rate
               .mul(BigNumber.from(losSpaceModifier.valueOneof.ratio.p))
               .div(BigNumber.from(losSpaceModifier.valueOneof.ratio.q));
             break;
           case 'fixed':
-            total = BigNumber.from(losSpaceModifier.valueOneof.fixed);
+            rate = BigNumber.from(losSpaceModifier.valueOneof.fixed);
             break;
           default:
         }
       }
     }
 
+    return rate;
+  };
+
+  // Apply DayOfWeekRateModifier modifier
+  static applyDayOfWeekModifier = async (
+    rate: BigNumber,
+    { modifiers, facilityModifiers }: QuoteRepositories,
+    day: DateTime
+  ): Promise<BigNumber> => {
+    let dowSpaceModifier: undefined | DayOfWeekRateModifier;
+
+    try {
+      dowSpaceModifier = (await modifiers.getModifier(
+        'day_of_week'
+      )) as DayOfWeekRateModifier;
+    } catch (e) {
+      if (e.status !== 404) {
+        throw e;
+      }
+
+      try {
+        dowSpaceModifier = (await facilityModifiers.getModifier(
+          'day_of_week'
+        )) as DayOfWeekRateModifier;
+      } catch (e) {
+        if (e.status !== 404) {
+          throw e;
+        }
+      }
+    }
+
+    if (dowSpaceModifier) {
+      const modifier = dowSpaceModifier[
+        day.weekdayShort.toLocaleLowerCase()
+      ] as DayOfWeekRateModifierElement;
+
+      if (modifier) {
+        switch (modifier.valueOneof.oneofKind) {
+          case 'ratio':
+            rate = rate
+              .mul(BigNumber.from(modifier.valueOneof.ratio.p))
+              .div(BigNumber.from(modifier.valueOneof.ratio.q));
+            break;
+          case 'fixed':
+            rate = BigNumber.from(modifier.valueOneof.fixed);
+            break;
+          default:
+        }
+      }
+    }
+
+    return rate;
+  };
+
+  // Apply OccupancyRateModifier modifier
+  static applyOccupancyModifier = async (
+    rate: BigNumber,
+    { modifiers, facilityModifiers }: QuoteRepositories,
+    numPaxAdult: number,
+    numPaxChild?: number
+  ): Promise<BigNumber> => {
+    let occupancySpaceModifier: undefined | OccupancyRateModifier;
+
+    try {
+      occupancySpaceModifier = (await modifiers.getModifier(
+        'occupancy'
+      )) as OccupancyRateModifier;
+    } catch (e) {
+      if (e.status !== 404) {
+        throw e;
+      }
+
+      try {
+        occupancySpaceModifier = (await facilityModifiers.getModifier(
+          'occupancy'
+        )) as OccupancyRateModifier;
+      } catch (e) {
+        if (e.status !== 404) {
+          throw e;
+        }
+      }
+    }
+
+    if (occupancySpaceModifier) {
+      const totalNumberOccupants = (numPaxAdult || 1) + (numPaxChild || 0);
+
+      switch (occupancySpaceModifier.valueOneof.oneofKind) {
+        case 'ratio':
+          rate = rate.add(
+            BigNumber.from(totalNumberOccupants)
+              .sub(BigNumber.from(1))
+              .mul(
+                rate
+                  .mul(
+                    BigNumber.from(occupancySpaceModifier.valueOneof.ratio.p)
+                  )
+                  .div(
+                    BigNumber.from(occupancySpaceModifier.valueOneof.ratio.q)
+                  )
+                  .sub(rate)
+              )
+          );
+          break;
+        case 'fixed':
+          rate = rate.add(
+            BigNumber.from(totalNumberOccupants)
+              .sub(BigNumber.from(1))
+              .mul(BigNumber.from(occupancySpaceModifier.valueOneof.fixed))
+          );
+          break;
+        default:
+      }
+    }
+
+    return rate;
+  };
+
+  // Returns a quote for the Ask
+  public quote = async (
+    facilityId: string,
+    spaceId: string,
+    ask: Ask
+  ): Promise<BigNumber> => {
+    if (!ask.checkIn) {
+      throw new Error('Invalid checkIn date');
+    }
+
+    if (!ask.checkOut) {
+      throw new Error('Invalid checkOut date');
+    }
+
+    const repositories: QuoteRepositories = {
+      rates: new SpaceRateRepository(facilityId, spaceId),
+      modifiers: new SpaceModifierRepository(facilityId, spaceId),
+      facilityModifiers: new FacilityModifierRepository(facilityId)
+    };
+    let total = BigNumber.from(0);
+
+    // Iterate through days from ask.checkIn to ask.checkOut
+    for await (const value of this.getQuoteIterator(repositories, ask)) {
+      total = total.add(value);
+    }
+
+    // Apply length_of_stay modifier, in order of priority
+    total = await QuoteService.applyLosRateModifier(
+      total,
+      repositories,
+      ask.checkIn as Date,
+      ask.checkOut as Date
+    );
+
     return total;
   };
 
   // Iterates through days from checkIn to checkOut
-  public getQuoteIterator = (
-    {
-      ratesRepository,
-      spaceModifiersRepository,
-      facilityModifierRepository
-    }: QuoteRepositories,
-    checkIn: Date,
-    checkOut: Date
-  ) => ({
+  public getQuoteIterator = (repositories: QuoteRepositories, ask: Ask) => ({
     [Symbol.asyncIterator]() {
-      const end = DateTime.fromObject(checkOut);
+      const end = DateTime.fromObject(ask.checkOut as Date);
       let days = 0;
 
       return {
         next: async () => {
-          const day = DateTime.fromObject(checkIn).plus({ days: days++ });
+          const day = DateTime.fromObject(ask.checkIn as Date).plus({
+            days: days++
+          });
 
           // Get the ‘base’ rate, in order of priority
-          let baseRate: Rates | null;
-
-          try {
-            baseRate = await ratesRepository.getRate(
-              day.toFormat('yyyy-MM-dd') as FormattedDate
-            );
-          } catch (e) {
-            if (e.status !== 404) {
-              throw e;
-            }
-
-            baseRate = await ratesRepository.getRate('default');
-          }
-
-          if (!baseRate) {
-            throw new Error('Unable to get base rate for the space');
-          }
-
-          // Base price value
-          let value = BigNumber.from(baseRate.cost);
+          let adjustedRate = await QuoteService.getBaseRate(repositories, day);
 
           // Apply day_of_week modifier, in order of priority
-          let dowSpaceModifier: undefined | DayOfWeekRateModifier;
-
-          try {
-            dowSpaceModifier = (await spaceModifiersRepository.getModifier(
-              'day_of_week'
-            )) as DayOfWeekRateModifier;
-          } catch (e) {
-            if (e.status !== 404) {
-              throw e;
-            }
-
-            try {
-              dowSpaceModifier = (await facilityModifierRepository.getModifier(
-                'day_of_week'
-              )) as DayOfWeekRateModifier;
-            } catch (e) {
-              if (e.status !== 404) {
-                throw e;
-              }
-            }
-          }
-
-          if (dowSpaceModifier) {
-            const modifier = dowSpaceModifier[
-              day.weekdayShort.toLocaleLowerCase()
-            ] as DayOfWeekRateModifierElement;
-
-            if (modifier) {
-              switch (modifier.valueOneof.oneofKind) {
-                case 'ratio':
-                  value = value
-                    .mul(BigNumber.from(modifier.valueOneof.ratio.p))
-                    .div(BigNumber.from(modifier.valueOneof.ratio.q));
-                  break;
-                case 'fixed':
-                  value = BigNumber.from(modifier.valueOneof.fixed);
-                  break;
-                default:
-              }
-            }
-          }
+          adjustedRate = await QuoteService.applyDayOfWeekModifier(
+            adjustedRate,
+            repositories,
+            day
+          );
 
           // Apply occupancy modifier, in order of priority
-          // let occupancySpaceModifier: undefined | OccupancyRateModifier;
-
-          // try {
-          //   occupancySpaceModifier = await spaceModifiersRepository.getModifier('occupancy') as OccupancyRateModifier;
-          // } catch (e) {
-          //   if (e.status !== 404) {
-          //     throw e;
-          //   }
-
-          //   try {
-          //     occupancySpaceModifier = await facilityModifierRepository.getModifier('occupancy') as OccupancyRateModifier;
-          //   } catch (e) {
-          //     if (e.status !== 404) {
-          //       throw e;
-          //     }
-          //   }
-          // }
-
-          // if (occupancySpaceModifier) {}
+          adjustedRate = await QuoteService.applyOccupancyModifier(
+            adjustedRate,
+            repositories,
+            ask.numPaxAdult,
+            ask.numPaxChild
+          );
 
           return {
-            value,
+            value: adjustedRate,
             done: day >= end
           };
         }
--- test/quote.spec.ts
+++ test/quote.spec.ts
@@ -6,7 +6,8 @@ import { Date } from '../src/proto/date';
 import {
   Rates,
   DayOfWeekRateModifier,
-  LOSRateModifier
+  LOSRateModifier,
+  OccupancyRateModifier
 } from '../src/proto/lpms';
 import { SpaceRateRepository } from '../src/repositories/ItemRateRepository';
 import {
@@ -14,8 +15,11 @@ import {
   SpaceModifierRepository
 } from '../src/repositories/ModifierRepository';
 
-import quoteService, { QuoteRepositories } from '../src/services/QuoteService';
-import { BigNumber } from 'ethers';
+import quoteService, {
+  QuoteRepositories,
+  QuoteService
+} from '../src/services/QuoteService';
+import { BigNumber, BigNumberish } from 'ethers';
 
 const createDate = (year: number, month: number, day: number): Date => ({
   year,
@@ -25,6 +29,32 @@ const createDate = (year: number, month: number, day: number): Date => ({
 
 const formattedDate = (date: Date) =>
   DateTime.fromObject(date).toFormat('yyyy-MM-dd') as FormattedDate;
+
+const calcRatio = (value: BigNumberish, p: number, q: number) => {
+  if (!BigNumber.isBigNumber(value)) {
+    value = BigNumber.from(value);
+  }
+  return value.mul(BigNumber.from(p)).div(BigNumber.from(q));
+};
+
+const calcOccupancyRatio = (
+  value: BigNumberish,
+  totalNumberOccupants: number,
+  p: number,
+  q: number
+) => {
+  if (!BigNumber.isBigNumber(value)) {
+    value = BigNumber.from(value);
+  }
+  return value.add(
+    BigNumber.from(totalNumberOccupants)
+      .sub(BigNumber.from(1))
+      .mul(
+        // perOccupantDelta
+        value.mul(BigNumber.from(p)).div(BigNumber.from(q)).sub(value)
+      )
+  );
+};
 
 describe('QuoteService', () => {
   const facilityId = '0x1234567890';
@@ -53,20 +83,20 @@ describe('QuoteService', () => {
       }
     }
   };
-  const losModifierRatioLt: LOSRateModifier = {
-    condition: 0,
-    los: 3,
-    valueOneof: {
-      oneofKind: 'ratio',
-      ratio: {
-        p: 150,
-        q: 100
-      }
-    }
-  };
+  // const losModifierRatioLt: LOSRateModifier = {
+  //   condition: 0,
+  //   los: 3,
+  //   valueOneof: {
+  //     oneofKind: 'ratio',
+  //     ratio: {
+  //       p: 150,
+  //       q: 100
+  //     }
+  //   }
+  // };
   const losModifierRatioGt: LOSRateModifier = {
-    condition: 3,
-    los: 4,
+    condition: 3, // GTE
+    los: 3,
     valueOneof: {
       oneofKind: 'ratio',
       ratio: {
@@ -75,81 +105,202 @@ describe('QuoteService', () => {
       }
     }
   };
-  const losModifierFixed: LOSRateModifier = {
-    condition: 0,
-    los: 2,
+  // const losModifierFixed: LOSRateModifier = {
+  //   condition: 0,
+  //   los: 2,
+  //   valueOneof: {
+  //     oneofKind: 'fixed',
+  //     fixed: 200
+  //   }
+  // };
+  const occupancyModifierRatio: OccupancyRateModifier = {
     valueOneof: {
-      oneofKind: 'fixed',
-      fixed: 200
+      oneofKind: 'ratio',
+      ratio: {
+        p: 110,
+        q: 100
+      }
     }
   };
+  const ask: Ask = {
+    checkIn: createDate(2022, 8, 1),
+    checkOut: createDate(2022, 8, 5),
+    numPaxAdult: 2,
+    numPaxChild: 1,
+    numSpacesReq: 2
+  };
+  const luckyDay: Date = createDate(2022, 8, 3);
   let repos: QuoteRepositories;
-  let ask: Ask;
-  let luckyDay: Date;
+  let reposClear: QuoteRepositories;
 
   before(async () => {
-    ask = {
-      checkIn: createDate(2022, 8, 1),
-      checkOut: createDate(2022, 8, 5),
-      numPaxAdult: 2,
-      numPaxChild: 1,
-      numSpacesReq: 2
-    };
-    luckyDay = createDate(2022, 8, 3);
     repos = {
-      ratesRepository: new SpaceRateRepository(facilityId, spaceId),
-      spaceModifiersRepository: new SpaceModifierRepository(
-        facilityId,
-        spaceId
-      ),
-      facilityModifierRepository: new FacilityModifierRepository(facilityId)
+      rates: new SpaceRateRepository(facilityId, spaceId),
+      modifiers: new SpaceModifierRepository(facilityId, spaceId),
+      facilityModifiers: new FacilityModifierRepository(facilityId)
     };
-    await repos.ratesRepository.setRateDefault(normalRate);
-    await repos.ratesRepository.setRate(formattedDate(luckyDay), luckyRate);
-    await repos.spaceModifiersRepository.setModifier(
-      'day_of_week',
-      dowModifier
-    );
-    await repos.spaceModifiersRepository.setModifier(
-      'length_of_stay',
-      losModifierRatioGt
-    );
+    reposClear = {
+      rates: new SpaceRateRepository(facilityId + '1', spaceId + '1'),
+      modifiers: new SpaceModifierRepository(facilityId + '1', spaceId + '1'),
+      facilityModifiers: new FacilityModifierRepository(facilityId + '1')
+    };
+    await repos.rates.setRateDefault(normalRate);
+    await repos.rates.setRate(formattedDate(luckyDay), luckyRate);
+    await repos.modifiers.setModifier('day_of_week', dowModifier);
+    await repos.modifiers.setModifier('length_of_stay', losModifierRatioGt);
+    await repos.modifiers.setModifier('occupancy', occupancyModifierRatio);
+  });
+
+  describe('Static members', () => {
+    describe('#getBaseRate', () => {
+      it('should return base rate for the space', async () => {
+        expect(
+          await QuoteService.getBaseRate(
+            repos,
+            DateTime.fromObject(ask.checkIn as Date)
+          )
+        ).to.eq(BigNumber.from(normalRate.cost));
+      });
+
+      it('should throw is space and facility not configured with rate', async () => {
+        await expect(
+          QuoteService.getBaseRate(
+            reposClear,
+            DateTime.fromObject(ask.checkIn as Date)
+          )
+        ).to.be.rejectedWith('Unable to get "default" of rate level');
+      });
+    });
+
+    describe('#applyLosRateModifier', () => {
+      it('should apply LOSRateModifier modifier', async () => {
+        expect(
+          await QuoteService.applyLosRateModifier(
+            BigNumber.from(normalRate.cost),
+            repos,
+            ask.checkIn as Date,
+            ask.checkOut as Date
+          )
+        ).to.eq(
+          BigNumber.from(normalRate.cost)
+            .mul(BigNumber.from((losModifierRatioGt as any).valueOneof.ratio.p))
+            .div(BigNumber.from((losModifierRatioGt as any).valueOneof.ratio.q))
+        );
+      });
+
+      it('should NOT apply on out of rule', async () => {
+        expect(
+          await QuoteService.applyLosRateModifier(
+            BigNumber.from(normalRate.cost),
+            repos,
+            ask.checkIn as Date,
+            createDate(2022, 8, 2)
+          )
+        ).to.eq(BigNumber.from(normalRate.cost));
+      });
+    });
+
+    describe('#applyDayOfWeekModifier', () => {
+      it('should apply DayOfWeekRateModifier modifier', async () => {
+        expect(
+          await QuoteService.applyDayOfWeekModifier(
+            BigNumber.from(normalRate.cost),
+            repos,
+            DateTime.fromObject(ask.checkIn as Date)
+          )
+        ).to.eq(
+          BigNumber.from(normalRate.cost)
+            .mul(BigNumber.from((dowModifier as any).mon.valueOneof.ratio.p))
+            .div(BigNumber.from((dowModifier as any).mon.valueOneof.ratio.q))
+        );
+      });
+    });
+
+    describe('#applyOccupancyModifier', () => {
+      it('should apply OccupancyRateModifier modifier', async () => {
+        const totalNumberOccupants =
+          (ask.numPaxAdult || 1) + (ask.numPaxChild || 0);
+        expect(
+          await QuoteService.applyOccupancyModifier(
+            BigNumber.from(normalRate.cost),
+            repos,
+            ask.numPaxAdult,
+            ask.numPaxChild
+          )
+        ).to.eq(
+          calcOccupancyRatio(
+            BigNumber.from(normalRate.cost),
+            totalNumberOccupants,
+            (occupancyModifierRatio as any).valueOneof.ratio.p,
+            (occupancyModifierRatio as any).valueOneof.ratio.q
+          )
+        );
+      });
+    });
   });
 
   describe('#getQuote async iterator', () => {
     it('should iterate though dates', async () => {
+      const totalNumberOccupants =
+        (ask.numPaxAdult || 1) + (ask.numPaxChild || 0);
       let days = 0;
-      for await (const value of quoteService.getQuoteIterator(
-        repos,
-        ask.checkIn as Date,
-        ask.checkOut as Date
-      )) {
+      for await (const value of quoteService.getQuoteIterator(repos, ask)) {
         switch (days) {
           case 0:
             // Mon rule 10%
+            // 110% occupancy
             expect(value).to.eq(
-              BigNumber.from(normalRate.cost)
-                .mul(
-                  BigNumber.from((dowModifier.mon as any).valueOneof.ratio.p)
-                )
-                .div(
-                  BigNumber.from((dowModifier.mon as any).valueOneof.ratio.q)
-                )
+              calcOccupancyRatio(
+                // dow
+                calcRatio(
+                  normalRate.cost,
+                  (dowModifier.mon as any).valueOneof.ratio.p,
+                  (dowModifier.mon as any).valueOneof.ratio.q
+                ),
+                totalNumberOccupants,
+                (occupancyModifierRatio as any).valueOneof.ratio.p,
+                (occupancyModifierRatio as any).valueOneof.ratio.q
+              )
             );
             break;
           case 1:
             // Tue rule fixed 20
+            // 110% occupancy
             expect(value).to.eq(
-              BigNumber.from((dowModifier.tue as any).valueOneof.fixed)
+              calcOccupancyRatio(
+                // fixed 20
+                BigNumber.from((dowModifier.tue as any).valueOneof.fixed),
+                totalNumberOccupants,
+                (occupancyModifierRatio as any).valueOneof.ratio.p,
+                (occupancyModifierRatio as any).valueOneof.ratio.q
+              )
             );
             break;
           case 2:
             // Lucky rate 50
-            expect(value).to.eq(BigNumber.from(luckyRate.cost));
+            // 110% occupancy
+            expect(value).to.eq(
+              calcOccupancyRatio(
+                // Lucky 50
+                BigNumber.from(luckyRate.cost),
+                totalNumberOccupants,
+                (occupancyModifierRatio as any).valueOneof.ratio.p,
+                (occupancyModifierRatio as any).valueOneof.ratio.q
+              )
+            );
             break;
           case 3:
             // Normal rate 100
-            expect(value).to.eq(BigNumber.from(normalRate.cost));
+            // 110% occupancy
+            expect(value).to.eq(
+              calcOccupancyRatio(
+                // normal rate
+                BigNumber.from(normalRate.cost),
+                totalNumberOccupants,
+                (occupancyModifierRatio as any).valueOneof.ratio.p,
+                (occupancyModifierRatio as any).valueOneof.ratio.q
+              )
+            );
             break;
           default:
         }
@@ -161,7 +312,8 @@ describe('QuoteService', () => {
   describe('#quote', () => {
     it('should return quote', async () => {
       const quote = await quoteService.quote(facilityId, spaceId, ask);
-      console.log('Quote:', quote.toString());
+      // Stupid Check
+      expect(quote).to.eq(BigNumber.from(194));
     });
   });
 });
