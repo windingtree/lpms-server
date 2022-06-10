97cbb25487a57903f70e93b2591b7e71b20b3eb8
Eugene <galeaf11@gmail.com>
2022-06-10 20:10:02 +0600

    feat: ping pong service refactor
    and multi facility support

--- src/services/PingPongService.ts
+++ src/services/PingPongService.ts
@@ -1,62 +1,50 @@
-import { geoToH3 } from 'h3-js';
 import { utils } from 'ethers';
-
 import { eip712, utils as vUtils } from '@windingtree/videre-sdk';
-import { constants } from '@windingtree/videre-sdk/dist/cjs/utils';
-
-import facilityRepository from '../repositories/FacilityRepository';
-
 import log from './LogService';
 import WakuService from './WakuService';
 import WalletService from './WalletService';
-
 import { walletAccountsIndexes } from '../types';
-import { Facility } from '../proto/facility';
 import { Ping, Pong } from '../proto/pingpong';
-import { LatLng } from '../proto/latlng';
-
 import { typedDataDomain, videreConfig } from '../config';
-import ApiError from '../exceptions/ApiError';
 
-export default class PingPongService {
+const unsubscribeHandler: () => void = () => {
+  return;
+};
+
+interface Unsubscribe {
+  h3Index: string;
+  handler: typeof unsubscribeHandler;
+}
+
+export class PingPongService {
   protected waku: WakuService;
-  protected unsubscribe: () => void = () => {
-    return;
-  };
+
+  protected unsubscribes = new Map<string, Unsubscribe>();
+  public locsManaged = new Map<string, string[]>();
+  public facilityToLoc = new Map<string, string>();
 
   /**
-   * After database initialization, start the ping / pong service
-   * for a specified facility.
+   * Start the ping / pong service for a specified facility.
    */
-  public async start(facilityId: string): Promise<void> {
-    const metadata = await facilityRepository.getFacilityKey<Facility>(
-      facilityId,
-      'metadata'
-    );
-
-    if (metadata === null) {
-      throw ApiError.NotFound(
-        `Unable to find "metadata" of the facility: ${facilityId}`
-      );
-    }
-
-    const loc = metadata.location;
-
+  public async start(facilityId: string, h3Index: string): Promise<void> {
     if (!this.waku) this.waku = WakuService.getInstance();
 
-    if (loc) {
-      // get the h3 index to monitor from the location
-      const h3Index = geoToH3(
-        loc.latitude,
-        loc.longitude,
-        constants.DefaultH3Resolution
-      );
+    //store Maps
+    this.facilityToLoc.set(facilityId, h3Index);
+    if (this.locsManaged.has(h3Index)) {
+      const locs = this.locsManaged.get(h3Index);
+      locs?.push(facilityId);
+      this.locsManaged.set(h3Index, locs || []);
+    } else {
+      this.locsManaged.set(h3Index, [facilityId]);
+    }
 
-      // watch for pings
-      this.unsubscribe = await this.waku.makeWakuObserver(
-        async (message) => {
-          const msg = this.waku.processMessage(Ping, message);
-          if (msg) log.green(`Ping received with timestamp: ${msg.timestamp}`);
+    // watch for pings
+    const observer = await this.waku.makeWakuObserver(
+      async (message) => {
+        const msg = this.waku.processMessage(Ping, message);
+        if (msg) {
+          log.green(`Ping received with timestamp: ${msg.timestamp}`);
 
           // respond to the ping with a pong
           this.waku.sendMessage(
@@ -66,20 +54,45 @@ export default class PingPongService {
               eip712.pingpong.Pong,
               {
                 serviceProvider: utils.arrayify(facilityId),
-                loc: LatLng.toBinary(loc),
+                loc: utils.toUtf8Bytes(h3Index),
+                timestamp: msg.timestamp,
                 signature: utils.toUtf8Bytes('') // initially blank signature which gets filled in
               },
               await WalletService.getWalletByIndex(walletAccountsIndexes.BIDDER)
             ),
             vUtils.generateTopic({ ...videreConfig, topic: 'pong' }, h3Index)
           );
-        },
-        [vUtils.generateTopic({ ...videreConfig, topic: 'ping' }, h3Index)]
-      );
-    }
+        }
+      },
+      [vUtils.generateTopic({ ...videreConfig, topic: 'ping' }, h3Index)]
+    );
+
+    this.unsubscribes.set(facilityId, {
+      h3Index: h3Index,
+      handler: observer
+    });
   }
 
-  public async stop(): Promise<void> {
-    this.unsubscribe();
+  public async stop(facilityId: string): Promise<void> {
+    const unsubscribe = this.unsubscribes.get(facilityId);
+    if (unsubscribe) {
+      this.unsubscribes.delete(facilityId);
+      this.facilityToLoc.delete(facilityId);
+
+      let locs = this.locsManaged.get(unsubscribe.h3Index);
+      if (locs) {
+        locs = locs.filter((v) => v !== facilityId);
+
+        this.locsManaged.set(unsubscribe.h3Index, locs);
+        if (this.locsManaged.get(unsubscribe.h3Index)?.length === 0) {
+          this.locsManaged.delete(unsubscribe.h3Index);
+        }
+        1;
+      }
+
+      unsubscribe.handler();
+    }
   }
 }
+
+export default new PingPongService();
