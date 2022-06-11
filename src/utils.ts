80e405613d2e175c9326a70c0d47ed8458bb7183
Eugene <galeaf11@gmail.com>
2022-06-11 12:13:08 +0600

    feat: ping pong service comment fixes
    Pong timestamp - current timestamp

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
+import { Timestamp } from './proto/timestamp';
+
 export function convertDaysToSeconds(days: number) {
   return days * 60 * 60 * 24;
+}
+//todo relocate to videre-sdk
+export function getCurrentTimestamp(): Timestamp {
+  //todo replace to vedere sdk
+  const timeMS = Date.now();
+
+  return {
+    seconds: BigInt(Math.floor(timeMS / 1000)),
+    nanos: (timeMS % 1000) * 1e6
+  };
 }
