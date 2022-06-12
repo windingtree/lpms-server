568c8e49b7898a48fe65d27ba0a1146c06080cb7
mfw78 <mfw78@protonmail.com>
2022-06-12 07:59:06 +0000

    feat: 🎸 auctioneer service

--- src/services/AuctioneerService.ts
+++ src/services/AuctioneerService.ts
@@ -1,21 +1,148 @@
-export class AuctioneerService {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  constructor() {
-    // Listen to waku and contentTopics from where when listening for a an Ask.
-    // Monitor Stays events, filtering by facilityId on chain for any Deals that have been done.
-  }
+import { BigNumber, constants, utils } from 'ethers';
+import { eip712, utils as vUtils } from '@windingtree/videre-sdk';
+import WakuService from './WakuService';
+import WalletService from './WalletService';
+import { walletAccountsIndexes } from '../types';
 
-  public async start(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      //... start logic
-      resolve();
+import { staysDataDomain, videreConfig } from '../config';
+import { generateBidLine } from '../utils';
+import { FacilityServiceInterface } from './interfaces/FacilityServiceInterface';
+import { AskWrapper, BidLine, Bids, BidWrapper } from '../proto/bidask';
+import { Ask } from '../proto/ask';
+import { SearchService } from './SearchService';
+import { QuoteService } from './QuoteService';
+import { ask as staysEIP712 } from '@windingtree/stays-models/dist/cjs/eip712';
+
+const unsubscribeHandler: () => void = () => {
+  return;
+};
+
+interface Unsubscribe {
+  h3Index: string;
+  handler: typeof unsubscribeHandler;
+}
+
+export class AuctioneerService implements FacilityServiceInterface {
+  protected waku: WakuService;
+
+  protected unsubscribes = new Map<string, Unsubscribe>();
+  public locsManaged = new Map<string, string[]>();
+  public facilityToLoc = new Map<string, string>();
+
+  /**
+   * Start the bid / ask service for a specified facility.
+   */
+  public async start(facilityId: string, h3Index: string): Promise<void> {
+    if (!this.waku) this.waku = WakuService.getInstance();
+
+    //store Maps
+    this.facilityToLoc.set(facilityId, h3Index);
+    if (this.locsManaged.has(h3Index)) {
+      const locs = this.locsManaged.get(h3Index);
+      locs?.push(facilityId);
+      this.locsManaged.set(h3Index, locs || []);
+    } else {
+      this.locsManaged.set(h3Index, [facilityId]);
+    }
+
+    // watch for asks
+    const observer = await this.waku.makeWakuObserver(
+      async (message) => {
+        const msg = this.waku.processMessage(AskWrapper, message);
+
+        // if we actually have an ask, we process it.
+        if (msg) {
+          const ask = Ask.fromBinary(msg.payload);
+
+          // first find if we have any spaces available
+          const spaces = await new SearchService(facilityId, ask).search();
+
+          const bidlines: BidLine[] = [];
+
+          const params = utils._TypedDataEncoder.hashStruct(
+            'Ask',
+            staysEIP712.Ask,
+            ask
+          );
+
+          for (const space of spaces) {
+            // TODO: currently assumes quote is xDAI
+            const quote = await new QuoteService().quote(
+              facilityId,
+              space,
+              ask
+            );
+
+            const bid = await generateBidLine(
+              await WalletService.getWalletByIndex(
+                walletAccountsIndexes.BIDDER
+              ),
+              utils.hexlify(msg.salt),
+              facilityId,
+              params,
+              [space],
+              5,
+              Math.floor(+new Date() / 1000) + 20 * 60,
+              constants.AddressZero,
+              quote.mul(BigNumber.from('10').pow('18'))
+            );
+
+            bidlines.push(bid);
+          }
+
+          // assemble the final bid message to be used in response
+          this.waku.sendMessage(
+            BidWrapper,
+            await vUtils.createSignedMessage<BidWrapper>(
+              staysDataDomain,
+              eip712.bidask.BidWrapper,
+              {
+                serviceProvider: utils.arrayify(facilityId),
+                payload: Bids.toBinary({ bids: bidlines }),
+                askDigest: utils.arrayify(
+                  utils.keccak256(
+                    utils.defaultAbiCoder.encode(
+                      ['bytes32', 'bytes32'],
+                      [msg.salt, params]
+                    )
+                  )
+                ),
+                signature: utils.toUtf8Bytes('')
+              },
+              await WalletService.getWalletByIndex(walletAccountsIndexes.BIDDER)
+            ),
+            vUtils.generateTopic({ ...videreConfig, topic: 'bid' }, h3Index)
+          );
+        }
+      },
+      [vUtils.generateTopic({ ...videreConfig, topic: 'ask' }, h3Index)]
+    );
+
+    this.unsubscribes.set(facilityId, {
+      h3Index: h3Index,
+      handler: observer
     });
   }
 
-  public async stop(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      //... stop logic
-      resolve();
-    });
+  public async stop(facilityId: string): Promise<void> {
+    const unsubscribe = this.unsubscribes.get(facilityId);
+    if (unsubscribe) {
+      this.unsubscribes.delete(facilityId);
+      this.facilityToLoc.delete(facilityId);
+
+      let locs = this.locsManaged.get(unsubscribe.h3Index);
+      if (locs) {
+        locs = locs.filter((v) => v !== facilityId);
+
+        this.locsManaged.set(unsubscribe.h3Index, locs);
+        if (this.locsManaged.get(unsubscribe.h3Index)?.length === 0) {
+          this.locsManaged.delete(unsubscribe.h3Index);
+        }
+      }
+
+      unsubscribe.handler();
+    }
   }
 }
+
+export default new AuctioneerService();
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { eip712 } from '@windingtree/videre-sdk';
+import { BigNumber, utils, Wallet } from 'ethers';
+import { staysDataDomain } from './config';
 import { Timestamp } from './proto/timestamp';
 import { providers } from 'ethers';
 import {
@@ -10,6 +13,7 @@ import { walletAccountsIndexes } from './types';
 export function convertDaysToSeconds(days: number) {
   return days * 60 * 60 * 24;
 }
+
 //todo relocate to videre-sdk
 export function getCurrentTimestamp(): Timestamp {
   const timeMS = Date.now();
@@ -62,4 +66,53 @@ export async function verifyFacilityBidder(
 
 export function getContractRole(role: walletAccountsIndexes): number {
   return role + 1;
+}
+
+// TODO: upstream this to `videre-sdk`
+export async function generateBidLine(
+  wallet: Wallet,
+  salt: string,
+  which: string,
+  params: string,
+  spaceIds: string[],
+  limit: number,
+  expiry: number,
+  gem: string,
+  wad: BigNumber
+) {
+  const items = spaceIds.map((v) => utils.arrayify(v));
+  return {
+    limit: limit,
+    expiry: expiry,
+    items: items,
+    terms: [],
+    options: undefined,
+    cost: [
+      {
+        gem: gem,
+        wad: wad.toString()
+      }
+    ],
+    signature: utils.arrayify(
+      await wallet._signTypedData(staysDataDomain, eip712.bidask.Bid, {
+        salt: salt,
+        limit: limit,
+        expiry: expiry,
+        which: which,
+        params: params,
+        items: items,
+        terms: [],
+        options: {
+          items: [],
+          terms: []
+        },
+        cost: [
+          {
+            gem: gem,
+            wad: wad
+          }
+        ]
+      })
+    )
+  };
 }
